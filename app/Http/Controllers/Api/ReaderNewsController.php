1b9600099b55a874fa966cc13e03f8d77ba5a382
Charly <schwarz-fischer@hotmail.com>
2023-04-18 20:27:07 -0300

    Documentar con swagger-l5

--- app/Http/Controllers/Api/ReaderNewsController.php
+++ app/Http/Controllers/Api/ReaderNewsController.php
@@ -4,10 +4,37 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\News;
-use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
 
 class ReaderNewsController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/news/{id}/readers",
+     *     summary="Get news with readers",
+     *     tags={"News"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of news",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="News with readers",
+     *         @OA\JsonContent(ref="#/components/schemas/News")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Not found",
+     *         @OA\JsonContent(ref="#/components/schemas/Error")
+     *     ),
+     * )
+     */
+
     public function getNewsWithReaders($id)
     {
         $news = News::with('readers')->findOrFail($id);
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,56 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="CRUD de Noticias y Lectores",
+     *      description="Documentacion de API para Noticias y Lectores",
+     *      @OA\Contact(
+     *          email="carlos@carlos"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      ),
+     * )
+     * @OA\Schema(
+     *     schema="News",
+     *     required={"id", "title", "content", "author"},
+     *     @OA\Property(
+     *         property="id",
+     *         type="integer",
+     *         description="The unique identifier of the news item"
+     *     ),
+     *     @OA\Property(
+     *         property="title",
+     *         type="string",
+     *         description="Titulo de las Noticias"
+     *     ),
+     *     @OA\Property(
+     *         property="content",
+     *         type="text",
+     *         description="Contenido de las noticias"
+     *     ),
+     *     @OA\Property(
+     *         property="author",
+     *         type="string",
+     *         description="Autor de las noticias"
+     *     )
+     * )
+     *
+     * @OA\Schema(
+     *     schema="Error",
+     *     required={"message"},
+     *     @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="Mensaje de error"
+     *     )
+     * )
+     */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
